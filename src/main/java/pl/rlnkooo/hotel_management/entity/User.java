bd6c6587ffcd88332c189ba07217f7423e30ad07
Artur Rolak <rolakartur96@gmail.com>
2024-10-16 16:04:03 +0200

    Added DTOs for Entity classes

--- src/main/java/pl/rlnkooo/hotel_management/entity/User.java
+++ src/main/java/pl/rlnkooo/hotel_management/entity/User.java
@@ -30,8 +30,12 @@ public class User implements UserDetails {
     @NotBlank(message = "Phone Number is required")
     private String phoneNumber;
 
+    @NotBlank(message = "Password is required")
     private String password;
+
     private String role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookings = new ArrayList<>();
 
     @Override
