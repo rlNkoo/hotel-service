69b7dec4f102bfa199a230568b444906e0090925
Artur Rolak <rolakartur96@gmail.com>
2024-10-21 18:14:37 +0200

    JwtUtils finished

---
+++ src/main/java/pl/rlnkooo/hotel_management/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package pl.rlnkooo.hotel_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.rlnkooo.hotel_management.exception.OurException;
+import pl.rlnkooo.hotel_management.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not found!"));
+    }
+}
---
+++ src/test/java/pl/rlnkooo/hotel_management/JwtSecretMakerTest.java
@@ -0,0 +1,18 @@
+package pl.rlnkooo.hotel_management;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class JwtSecretMakerTest {
+
+    @Test
+    public void generateSecretKey() {
+        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        System.out.printf("\nKey = [%s]\n", encodedKey);
+    }
+}
